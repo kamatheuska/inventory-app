018f6d62833d4f9cd7095aaadbc19899f9cdd8f8
Nicolas Ramirez <nico9017@gmail.com>
2022-12-28 12:20:18 +0100

    types: add auth types

---
+++ packages/types/auth.types.ts
@@ -0,0 +1,36 @@
+import { Document, Model, Types } from 'mongoose';
+
+export interface UserHashedCredentials {
+    username: string;
+    hashedPassword: string;
+}
+
+export interface UserCredentials {
+    username: string;
+    password: string;
+}
+
+export interface IUser {
+    username: string;
+    password: string;
+    tokens: IToken[];
+}
+
+export type UserModel = Model<IUser>;
+
+export type UserDocument = (Document<unknown, any, IUser> & IUser & {
+    _id: Types.ObjectId;
+}) | null;
+
+
+export interface IToken {
+    hash: string;
+    createdAt?: Date;
+}
+
+export interface TokenPayload {
+    username: string;
+    password: string;
+}
+
+export type TokenModel = Model<IToken>;
--- packages/types/index.ts
+++ packages/types/index.ts
@@ -1,3 +1,4 @@
 export * from './movement.types'
 export * from './ingredient.types'
 export * from './storage-item.types'
+export * from './auth.types'
