e23989bd1adefe88b135583e4219ae4d058776ef
Nicolas Ramirez <nico9017@gmail.com>
2023-01-01 20:52:43 +0100

    api: clean types from not used mongodb vars

--- packages/api/src/app.types.ts
+++ packages/api/src/app.types.ts
@@ -13,13 +13,6 @@ export interface AppConfig {
     DISABLE_SIGNUP: boolean;
     JWT_SECRET: string;
     MONGODB_URI: string;
-    MONGODB_PROTOCOL: string;
-    MONGODB_USER: string;
-    MONGODB_PASSWORD: string;
-    MONGODB_HOST: string;
-    MONGODB_PORT: number;
-    MONGODB_COLLECTION: string;
-    MONGODB_AUTH_SOURCE: string;
     MONGODB_SERVER_SELECTION_TIMEOUT_MS: number;
     NODE_ENV: string;
     PORT: string;
